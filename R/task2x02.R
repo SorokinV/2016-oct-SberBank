0edc336868ad70799f7ff1da018f9fb19403cfee
boba2 <sorokinv@hotmail.com>
2016-10-20 20:33:07 +1000

    task2 initial commit before estimation parameters

--- R/task2x02.R
+++ R/task2x02.R
@@ -1,18 +1,25 @@
 require(plyr)
-require(tidyr)
-
+#require(tidyr)
 require(forecast)
 
 #
-# get minus transaction
+# get minus transaction and build days
 #
 
 trsm = subset(trs,trs$amount<0.0)
+
+dd = regexpr("[0-9]+",trs$tr_datetime); 
+ds = as.numeric(substr(trs$tr_datetime,dd,dd+attr(dd,"match.length")-1)); 
+ds.max = max(ds)
+
+
 dsm  = as.numeric(ds[trs$amount<0.0])
 trsm$day = dsm
-dsmw = dsm %% 7
+#dsmw = dsm %% 7
 
-agg.mcc2 = 
+rm(dd,ds,dsm)
+
+agg2.mcc = 
   ddply(trsm,.(mcc_code,day),summarise,
         ssum=sum(amount),
         nn=length(amount),
@@ -22,9 +29,9 @@ agg.mcc2 =
 # -----------------------------------------
 # build grid for mcc-code and day
 
-df.temp.0 = data.frame("day"=c(min(agg.mcc2$day):max(agg.mcc2$day)))
+df.temp.0 = data.frame("day"=c(min(agg2.mcc$day):max(agg2.mcc$day)))
 iCount    = 0
-for (i in sort(unique(agg.mcc2$mcc_code))) {
+for (i in sort(unique(agg2.mcc$mcc_code))) {
   df.temp.1 = df.temp.0;
   df.temp.1$mcc_code = i;
   if (iCount==0) {
@@ -39,18 +46,18 @@ rm(iCount,df.temp.0,df.temp.1)
 
 # build timeSeries from all days min-max
 
-agg.mcc2.ts = merge(df[c(2,1)],agg.mcc2[c(1,2,3)],all.x=TRUE); 
-agg.mcc2.ts$ssum[is.na(agg.mcc2.ts$ssum)]=0.0; str(agg.mcc2.ts)
+agg2.mcc.ts = merge(df[c(2,1)],agg2.mcc[c(1,2,3)],all.x=TRUE); 
+agg2.mcc.ts$ssum[is.na(agg2.mcc.ts$ssum)]=0.0; str(agg2.mcc.ts)
 
 rm(df)
 
 # build stl for all mcc code
 
-df = data.frame()
+task2 = data.frame()
 
-for (mcc in unique(agg.mcc2.ts$mcc_code)) {
-  zz           = ts(agg.mcc2.ts$ssum[agg.mcc2.ts$mcc_code==mcc],start=0,frequency = 30)
-  zz.stl       = stlm(log(500-zz),s.window = 26,method="arima")
+for (mcc in unique(agg2.mcc.ts$mcc_code)) {
+  zz           = ts(agg2.mcc.ts$ssum[agg2.mcc.ts$mcc_code==mcc],start=0,frequency = 30)
+  zz.stl       = stlm(log(500-zz),s.window = 24,method="arima")
 #  zz.stl       = stlm(log(500-zz),s.window = "periodic",method="arima")
   #zz.stl       = stlm(log(500-zz))
   #  zz.stl       = stlm(zz)
@@ -59,19 +66,19 @@ for (mcc in unique(agg.mcc2.ts$mcc_code)) {
 #  dff          = data.frame("volume"=as.numeric(zz.for$mean))
   dff$mcc_code = mcc
   dff$day      = c(1:30)
-  df           = rbind(df,dff)
+  task2        = rbind(task2,dff)
   
 }
 
-str(dff)
+str(dff); str(task2)
 
 rm(zz,zz.stl,zz.for,dff)
 
-df$day = df$day+max(agg.mcc2.ts$day)
+task2$day = task2$day+max(agg2.mcc.ts$day)
 
 #---------------------------------------------
 
 nStep   = strftime(Sys.time(),"%Y%m%d-%H%M%S")
 outfile = paste("./Result/task2-",nStep,'.csv',sep='') 
-write.csv(df[c(2,3,1)],file=outfile,quote=FALSE,row.names=FALSE)
+write.csv(task2[c(2,3,1)],file=outfile,quote=FALSE,row.names=FALSE)
 
